be48e4007696fe07b2129fe34f49475fe12900ae
Sergey Chikalin <gavr013@gmail.com>
2014-08-23 12:37:01 +0400

    Added NGUI package; Added leg-slider volume control to scene;

---
+++ Unity/Assets/VolumeControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeControl : MonoBehaviour {
+
+	MicControlC micControl;
+	UISlider slider;
+
+	public void Start()
+	{
+		var mainCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+		micControl = mainCamera.GetComponent<MicControlC> ();
+		slider = GetComponent<UISlider> ();
+	}
+
+	public void Update()
+	{
+		var volume = slider.sliderValue;
+		micControl.sourceVolume = volume * 100;
+	}
+}
